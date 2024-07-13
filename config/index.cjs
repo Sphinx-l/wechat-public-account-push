cb98d1a9041324b8f0470c1cc8977e25360f0dd3
Sphinx-l <87009423+Sphinx-l@users.noreply.github.com>
2024-07-13 23:05:29 +0800

    Update index.cjs
    去掉空格格式

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oD0Yo6S6T36fxpKpok1QqaVZ6apY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	l3gDMeya_C8WoYh2SLXlzP0okY1v9VUNDpFwsnvbMZE',
+      useTemplateId: 'l3gDMeya_C8WoYh2SLXlzP0okY1v9VUNDpFwsnvbMZE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -52,7 +52,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	LzMdLvIGrkJljc30GfZzTdXvr-ag4vWtpigR8c6VxOs',
+  CALLBACK_TEMPLATE_ID: 'LzMdLvIGrkJljc30GfZzTdXvr-ag4vWtpigR8c6VxOs',
 
   CALLBACK_USERS: [
     {
