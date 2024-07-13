2b849050b40f94e4cc467341d35910010c73e25d
Sphinx-l <87009423+Sphinx-l@users.noreply.github.com>
2024-07-13 21:48:05 +0800

    Update index.cjs
    修改配置wxid

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '晚晚',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '94f7ee2b0cbb48958f4ea0cb846404be',
+      id: 'oD0Yo6ap2uyfPcNyRvYp_cXx-UBk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
