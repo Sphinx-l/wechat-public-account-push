330ac1d9ba709e15589d0b364bd6f96fa590b7f2
Sphinx-l <87009423+Sphinx-l@users.noreply.github.com>
2024-07-13 23:15:57 +0800

    Update index.cjs
    1

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oD0Yo6S6T36fxpKpok1QqaVZ6apY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'l3gDMeya_C8WoYh2SLXlzP0okY1v9VUNDpFwsnvbMZE',
+      useTemplateId: '	r1oFww4zQwY0TI_mc7ozz5rUXNtcK1I83Q4gitfG5jA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
