bb36a7e55219c4423c7d700be12928a27daaeca2
Sphinx-l <87009423+Sphinx-l@users.noreply.github.com>
2024-07-13 23:00:11 +0800

    Update index.cjs
    配置修改

--- config/index.cjs
+++ config/index.cjs
@@ -14,27 +14,27 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '浙江',
-  CITY: '杭州',
+  PROVINCE: '北京',
+  CITY: '北京',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '晚晚',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oD0Yo6ap2uyfPcNyRvYp_cXx-UBk',
+      id: 'oD0Yo6S6T36fxpKpok1QqaVZ6apY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LBmA3eCn338McXNp9ZKx-YUfGoK1x8EvSJWouB-WkcE',
+      useTemplateId: '	l3gDMeya_C8WoYh2SLXlzP0okY1v9VUNDpFwsnvbMZE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2002', date: '11-20',
+          type: '*生日', name: '宝贝', year: '2002', date: '10-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2002', date: '11-20',
+          type: '生日', name: '晚晚', year: '2002', date: '11-20',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '06-24',
@@ -45,20 +45,20 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2024-06-24' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2026-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '	LzMdLvIGrkJljc30GfZzTdXvr-ag4vWtpigR8c6VxOs',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oD0Yo6ap2uyfPcNyRvYp_cXx-UBk',
     }
   ],
 
